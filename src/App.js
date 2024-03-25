d2fa7c96efc3e421f9760fc691f2724bf1085332
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2024-03-25 16:58:36 +0900

    code : HashRouter로 변경

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import InfoPage from "./pages/InfoPage";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/info" element={<InfoPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
