60cafe84debfc646ce556ea88daebe9620ccd909
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2024-03-27 17:10:09 +0900

    code : 홈페이지 초안 완성

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,23 @@
-import { HomePageBody, LogoImg, TitleArea, TitleEle } from "./HomePage.style";
+import { DownloadBtn, HeaderNavBar, HomePageBody, LogoImg, ScreenshotImg, Section1, TitleArea, TitleEle } from "./HomePage.style";
 import Logo from "../assets/logo.png";
+import ScreenShot from "../assets/screenshot.png";
 
 const HomePage = () => {
   return (
     <HomePageBody>
-      <LogoImg src={Logo} />
-      <TitleArea>
-        <TitleEle $seq={1}>EZ-Sequence</TitleEle>
-        <TitleEle $seq={2}>이지-시퀀스, 가장 쉬운 시퀀스 회로 프로그램</TitleEle>
-        <TitleEle $seq={3}>마우스 클릭으로 1분만에 회로도 그리기</TitleEle>
-      </TitleArea>
+      <HeaderNavBar>
+        <LogoImg src={Logo} />
+      </HeaderNavBar>
+
+      <Section1>
+        <TitleArea>
+          <TitleEle $seq={1}>EZ-Sequence</TitleEle>
+          <TitleEle $seq={2}>이지-시퀀스, 가장 쉬운 시퀀스 회로 프로그램</TitleEle>
+          <TitleEle $seq={3}>마우스 클릭으로 1분만에 회로도 그리기</TitleEle>
+          <DownloadBtn>무료 다운로드</DownloadBtn>
+        </TitleArea>
+        <ScreenshotImg src={ScreenShot} alt="screenshot" />
+      </Section1>
     </HomePageBody>
   );
 };
--- src/pages/HomePage.style.js
+++ src/pages/HomePage.style.js
@@ -1,25 +1,39 @@
 import { styled, css } from "styled-components";
 
 export const HomePageBody = styled.div`
-  height: 100vh;
-  width: 100vw;
   margin: 0;
+  padding-bottom: 60px;
+  overflow: hidden;
 
   background-color: #b0bde022;
+`;
+
+export const HeaderNavBar = styled.div`
+  height: 90px;
+  width: 100vw;
 `;
 
 export const LogoImg = styled.img`
   height: 45px;
   width: 48px;
-  margin-top: 20px;
-  margin-left: 22px;
-  margin-bottom: 60px;
+  padding: 22px;
 `;
+
+export const Section1 = styled.div`
+  display: flex;
+  padding-top: 110px;
+`;
+
+export const ScreenshotImg = styled.img`
+  width: 80%;
+  margin-top: -80px;
+  margin-left: 70px;
+`;
+
 
 export const TitleArea = styled.div`
   position: relative;
-  left: 120px;
-  width: 90%;
+  padding-left: 60px;
 `;
 
 export const TitleEle = styled.div`
@@ -28,32 +42,38 @@ export const TitleEle = styled.div`
       ? css`
           font-weight: 800;
           font-size: 80px;
+          line-height: 70px;
           color: #111;
         `
       : props.$seq === 2
       ? css`
-          margin-top: 5px;
+          margin-top: 25px;
           font-weight: 500;
           font-size: 40px;
           color: #11111160;
-          letter-spacing: -3px;
+          letter-spacing: -3.3px;
         `
       : props.$seq === 3
       ? css`
           margin-top: -1px;
           padding-left: 4px;
-          font-size: 29px;
+          font-size: 30px;
           font-weight: 500;
           color: #11111133;
-          letter-spacing: -1.5px;
+          letter-spacing: -2px;
         `
       : ``}
 `;
 
 export const DownloadBtn = styled.div`
-  border-radius: 12px;
+  display: inline-block;
+  margin: 35px 0 0 6px;
+  padding: 16px 70px;
+  border-radius: 8px;
   border: none;
   background-color: #333;
   color: white;
-  padding: 10px 30px;
+  font-size: 21px;
+  letter-spacing: -1px;
+  cursor: pointer;
 `;
