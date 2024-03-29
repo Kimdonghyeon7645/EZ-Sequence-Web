b302b5d2731b0c23f703aa149465536f4c06cd27
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2024-03-29 10:50:37 +0900

    code : 시간 더하기

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
+
 const InfoPage = () => {
+  const [inData, setInData] = useState("");
+  const [outData, setOutData] = useState("");
+
+  useEffect(() => {
+    const txtLi = inData.split("+");
+    const numLi = [];
+    for (var idx in txtLi) {
+      const [h, m, s] = txtLi[idx].split(":");
+      console.log(h, m,s);
+      if (h && m && s) numLi.push(parseInt(h) * 3600 + parseInt(m) * 60 + parseInt(s));
+    }
+
+    console.log(numLi);
+    if (numLi.length > 0) {
+      const numSum = numLi.reduce((a, b) => a+b);
+      setOutData(`${Math.floor(numSum / 3600)}:${Math.floor(numSum % 3600 / 60)}:${numSum % 60}`)
+    }
+  }, [inData]);
+
   return <div>
-    <div>정보여</div>
+    <input onChange={(e) => setInData(e.target.value)} />
+    <p>{outData}</p>
   </div>;
 };
 
